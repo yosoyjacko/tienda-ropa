53970e93201ce0786ef88ce0f03bdfa50f26e3e0
jacko <jorge_a.correa@hotmail.com>
2021-07-05 22:50:09 -0300

    creacion de card de product

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tienda-ropa
 ecommerce de una tienda de venta de ropa proyecto practica profesional
+
+
+framesworks instalados hasta ahora: material-ui/core
+material-ui/icons
--- ecommerce-ropa/src/App.js
+++ ecommerce-ropa/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <h1>Tienda ecommerce rama creada</h1>
+      <Product />
     </div>
   );
 }
